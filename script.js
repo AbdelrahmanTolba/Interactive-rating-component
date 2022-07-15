c9bf112d03f2d45ad5a3926e342c62db92c1f250
Abdelrahman Tolba <90225424+AbdelrahmanTolba@users.noreply.github.com>
2022-07-15 22:22:20 +0200

    some refactoring

--- script.js
+++ script.js
@@ -6,21 +6,19 @@ const yourRate = document.querySelector(".your__rate");
 const rateNum = document.querySelectorAll(".rate__num");
 
 submitBtn.addEventListener("click", function () {
-  rateNum.forEach((el, i) => {
-    if (el.classList.contains("selected")) {
+  rateNum.forEach((elm, i) => {
+    if (elm.classList.contains("selected")) {
       thankinSection.classList.remove("hidden");
       ratingSection.classList.add("hidden");
       yourRate.textContent = `${i + 1}`;
     } else return;
   });
 });
-rateNum.forEach((el) => {
-  el.addEventListener("click", function () {
-    if (!el.classList.contains("selected")) {
-      rateNum.forEach((el) => el.classList.remove("selected"));
-      el.classList.add("selected");
-    } else {
-      el.classList.remove("selected");
+rateNum.forEach((elm) => {
+  elm.addEventListener("click", function () {
+    if (!elm.classList.contains("selected")) {
+      rateNum.forEach((elm) => elm.classList.remove("selected"));
+      elm.classList.toggle("selected");
     }
   });
 });
